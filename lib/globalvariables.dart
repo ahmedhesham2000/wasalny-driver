c1d624e73c1e662dc3c0996a84665c73ebf2f8d5
ahmedhesham2000 <58794528+ahmedhesham2000@users.noreply.github.com>
2022-07-07 00:13:53 +0200

    Update globalvariables.dart

--- lib/globalvariables.dart
+++ lib/globalvariables.dart
@@ -7,7 +7,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wasalny_driver/datamodel/driver.dart';
 
-String mapKey='AIzaSyBWIR4xpMnYGOx4YtBltOk4MyE0iJGUlw0';
+String mapKey='Your API key';
 final CameraPosition googlePlex = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
   zoom: 14.4746,
